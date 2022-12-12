ae06f8dfe0472500bc00e39ff59ec6f86bdedca2
hjs6877 <hjs6877@gmail.com>
2022-12-12 14:13:51 +0900

    Modify post handlers

--- src/main/java/com/codestates/member/controller/MemberController.java
+++ src/main/java/com/codestates/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.codestates.member.controller;
 import com.codestates.member.dto.MemberDto;
 import com.codestates.member.mapper.MemberMapper;
 import com.codestates.member.service.MemberService;
+import com.codestates.utils.UriCreator;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
@@ -13,12 +14,14 @@ import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
+import java.net.URI;
 import java.util.List;
 
 @Validated
 @RestController
 @RequestMapping("/v12/members")
 public class MemberController {
+    private final static String MEMBER_DEFAULT_URL = "/v12/members";
     private final MemberService memberService;
     private final MemberMapper mapper;
 
@@ -28,13 +31,13 @@ public class MemberController {
     }
 
     @PostMapping
-    public ResponseEntity postMember(@Valid @RequestBody Mono<MemberDto.Post> requestBody) {
-        Mono<MemberDto.Response> result =
-                requestBody
-                        .flatMap(post -> memberService.createMember(mapper.memberPostToMember(post)))
-                        .map(member -> mapper.memberToMemberResponse(member));
-
-        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    public Mono<ResponseEntity> postMember(@Valid @RequestBody Mono<MemberDto.Post> requestBody) {
+        return requestBody
+                .flatMap(post -> memberService.createMember(mapper.memberPostToMember(post)))
+                .map(createdMember -> {
+                    URI location = UriCreator.createUri(MEMBER_DEFAULT_URL, createdMember.getMemberId());
+                    return ResponseEntity.created(location).build();
+                });
     }
 
     @PatchMapping("/{member-id}")
